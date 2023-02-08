c2c32814deb07e56d5d9a0aea0e5cfff371d5bbc
AdenSky <n.naghieh@gmail.com>
2023-02-08 14:33:11 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// alert("Hello")
+// Log to consolconsole.log("Hello world")
+
+/*
+console.log(1234)
+
+var a="NEDA"
+console.log(a)
+
+console.log([1,2,3,4,5])
+
+console.log({a:1, b:2})
+
+console.table({a:1, b:2})
+
+console.error("this is some error")
+
+*/
+
+
+// var name="Java Script"
+// console.log(name)
+
+// name="Neda"
+// console.log(name)
+
+// var vel;
+// vel = String(555);
+// console.log(vel);
+// console.log(typeof vel);
+// console.log(vel.length);
+
+
+var vel;
+vel = String (new Date());
+console.log(vel);
+console.log(typeof vel);
+console.log(vel.length);
